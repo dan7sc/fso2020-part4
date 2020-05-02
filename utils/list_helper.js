8a7d0e2a13e7fcd4aa226868efde009d7e3173c3
dan7sc <su7cheng@gmail.com>
2020-05-01 22:51:19 -0300

    Add solution to exercise 4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,32 @@ const totalLikes = (blogs) => {
   }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  let maxLikes = -1
+  let blogWithMostLikes = {}
+  blogs.forEach(blog => {
+    if (blog.likes > maxLikes) {
+      maxLikes = blog.likes
+      blogWithMostLikes = blog
+    }
+  })
+
+  delete blogWithMostLikes._id
+  delete blogWithMostLikes.url
+  delete blogWithMostLikes.__v
+  return blogWithMostLikes
+}
+
+// const favoriteBlog = (blogs) => {
+//   return Math.max(
+//     ...blogs.map(blog => {
+//       return blog.likes
+//     })
+//   )
+// }
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
