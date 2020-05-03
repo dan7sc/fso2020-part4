ca37397fe65102e9503dac121ad4bb3de0da05e4
dan7sc <su7cheng@gmail.com>
2020-05-03 11:24:19 -0300

    Add solution to exercise 4.15: bloglist expansion, step3

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const logger = require('./utils/logger')
 
 logger.info(`Connecting to database ${config.MONGODB_URI}`)
@@ -21,5 +22,6 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 module.exports = app
