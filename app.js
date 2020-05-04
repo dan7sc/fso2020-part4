161050fee32d4d7cb83d258df7974b35c20f3687
dan7sc <su7cheng@gmail.com>
2020-05-03 22:34:07 -0300

    Add solution to exercise 4.18: bloglist expansion, step6

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 
@@ -25,6 +26,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
